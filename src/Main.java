7cd30c508e16926d9e1421dca29fe547634c4791
nusubbotin <nu.subbotin@ya.ru>
2022-09-25 12:01:41 +0300

    Циклы. Часть 2. Домашнее задание - 1. Задание 2

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,23 @@ public class Main {
         }
     }
 
+    //Домашнее задание -1. Задача 2
+    public static void task1_2(){
+        System.out.println("Задание 2: ");
+        int num = 0;
+        while (num < 10) {
+            num++;
+            System.out.print(num + " ");
+        }
+        System.out.print("\n");
+
+        for (; num >= 1; num--) {
+            System.out.print(num + " ");
+        }
+    }
+
     public static void main(String[] args) {
         task1_1();
+        task1_2();
     }
 }
