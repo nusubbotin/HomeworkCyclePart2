117e55577ea9e95b36c83e61be5ff0eb93916a77
nusubbotin <nu.subbotin@ya.ru>
2022-09-25 11:59:30 +0300

    Циклы. Часть 2. Домашнее задание - 1. Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-     public static void main(String[] args) {
+    //Домашнее задание -1. Задача 1
+    public static void task1_1(){
+        int month = 0;
+        int addSummMoney = 15000;
+        float accumMoney = 0;
+        System.out.println("Задание 1: ");
+        while (accumMoney < 2_459_000) {
+            month++;
+            accumMoney = accumMoney * 1.12f + addSummMoney;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + accumMoney + " рублей");
+        }
+    }
 
+    public static void main(String[] args) {
+        task1_1();
     }
 }
